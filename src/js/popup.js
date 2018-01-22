c4d31fa11545c4833c7668a535ef814b4ae04846
Christopher DeCairos <cade@mozillafoundation.org>
2018-01-22 00:10:44 -0500

    implemented tab selection and switching

--- src/js/popup.js
+++ src/js/popup.js
@@ -7,25 +7,85 @@ class FastTab {
     constructor() {
         this._searchBox = document.querySelector('#search-box');
         this._resultsDiv = document.querySelector('#results-div');
+        this._selectedIndex = 0;
 
         this._searchBox.addEventListener('input', () => this.searchTabs());
+        this._searchBox.addEventListener('keyup', (...args) => this.processKeyUp(...args));
 
+        // hack to focus popup window and search box
         setTimeout(() => {
           document.querySelector('body').focus();
           setTimeout(() => {
             this._searchBox.focus();
           }, 0)
-        }, 0);
+        }, 50);
     }
 
-    switchToTab(tabId) {
+    set selectedIndex(newValue) {
+        const resultsLength = this._searchableTabs.length;
+
+        if (resultsLength === 0) {
+            return;
+        }
+
+        const lastIndex = this._selectedIndex;
+        this._selectedIndex = newValue;
+
+        // Selection wrapping
+        if (this.selectedIndex < 0) {
+            this._selectedIndex = resultsLength - 1;
+        } else if (this.selectedIndex === resultsLength) {
+            this._selectedIndex = 0
+        }
+
+        this._searchableTabs[lastIndex]._div.classList.remove('selected');
+        this._searchableTabs[this.selectedIndex]._div.classList.add('selected');
+    }
+
+    get selectedIndex() {
+        return this._selectedIndex;
+    }
+
+    set searchableTabs(tabs) {
+        this._searchableTabs = tabs;
+        this.clear();
+
+        if (!this._searchableTabs.length) {
+            console.log("No results");
+            return;
+        }
+
+        this._searchableTabs.forEach((...args) => this.renderTab(...args));
+        this.selectedIndex = 0;
+    }
+
+    switchToSelectedTab() {
+        const tabId = this._searchableTabs[this.selectedIndex].id;
         tabs.update(tabId, {
             active: true
-        });
+        })
+        .then(() => window.close());
+    }
+
+    processKeyUp(event) {
+        switch(event.key) {
+            case "ArrowUp":
+                this.selectedIndex--;
+                break;
+            case "ArrowDown":
+                this.selectedIndex++
+                break;
+            case "Enter":
+                this.switchToSelectedTab();
+                break;
+            case "Escape":
+                window.close();
+                break;
+        }
     }
 
     async searchTabs() {
-        this.display(await runtime.sendMessage(this._searchBox.value));
+        this.searchableTabs = await runtime.sendMessage(this._searchBox.value);
     }
 
     clear() {
@@ -34,25 +94,13 @@ class FastTab {
         }
     }
 
-    addResult(tab) {
-        const tabHit = document.createElement('div');
-        tabHit.classList.add('result-item');
-        tabHit.textContent = tab.title;
-        tabHit.setAttribute('data-tab-id', tab.id);
-        this._resultsDiv.appendChild(tabHit);
-    }
-
-    display(results) {
-        this.clear();
-
-        if (!results.length) {
-            console.log("No results");
-            return;
-        }
-
-        for(const tab of results) {
-            this.addResult(tab);
-        }
+    renderTab(tab, index) {
+        const tabDiv = document.createElement('div');
+        tabDiv.classList.add('result-item');
+        tabDiv.textContent = tab.title;
+        tabDiv.setAttribute('data-tab-id', tab.id);
+        this._resultsDiv.appendChild(tabDiv);
+        this._searchableTabs[index]._div = tabDiv;
     }
 }
 
