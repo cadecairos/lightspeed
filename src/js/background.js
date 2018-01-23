0525007ae75e695aee5c289db5c690828c2a58c5
Christopher DeCairos <cade@mozillafoundation.org>
2018-01-22 23:47:23 -0500

    Switch from jsSearch to fuzzy, refactor background script to better track tab state.
    Need to implement per-window tab switching. consider toggleable global tab switching, but not clear if focus can be handed to a different window

--- src/js/background.js
+++ src/js/background.js
@@ -1,37 +1,96 @@
-import JsSearch from 'js-search'
+import fuzzy from 'fuzzy'
 
-const { tabs, runtime } = browser;
+const { tabs, runtime, windows } = browser;
 
 class FastTabIndex {
     constructor() {
-        this.jsSearch = null;
+        // TODO: Per Window TabMaps
+        this.tabMap =  new Map();
 
-        tabs.onCreated.addListener(() => this.updateSearchIndex());
-        tabs.onAttached.addListener(() => this.updateSearchIndex());
-        tabs.onUpdated.addListener(() => this.updateSearchIndex());
-        tabs.onRemoved.addListener(() => this.updateSearchIndex());
+        tabs.onCreated.addListener((...args) => this.addTab(...args));
+        tabs.onAttached.addListener((...args) => this.onAttached(...args));
+        tabs.onDetached.addListener((...args) => this.onDetached(...args));
+        tabs.onUpdated.addListener((...args) => this.onUpdated(...args));
+        tabs.onRemoved.addListener((...args) => this.onRemoved(...args));
 
         browser.runtime.onMessage.addListener((...args) => {
             this.searchIndex(...args);
         });
     }
 
-    getTabs() {
-        return tabs.query({currentWindow: true});
+    get fuzzyOptions() {
+        return { extract: (tab) => tab.title };
     }
 
-    async updateSearchIndex() {
-        this.jsSearch = new JsSearch.Search('url');
-        this.jsSearch.addIndex('title');
-        this.jsSearch.addDocuments(await this.getTabs());
+    get tabMapValueArray() {
+        return Array.from(this.tabMap.values());
+    }
+
+    formatTab({id, url, title}) {
+        return {id, url, title};
+    }
+
+    addTab(tab) {
+        if (this.tabMap.has(tab.id)) {
+            return;
+        }
+
+        this.tabMap.set(tab.id, this.formatTab(tab));
+    }
+
+    async onAttached(tabId, attachInfo) {
+        if (this.tabMap.has(tabId)) {
+            return;
+        }
+
+        const tab = await tabs.get(tabId);
+
+        if (!tab || tab.windowId !== windows.WINDOW_ID_CURRENT) {
+            return;
+        }
+
+        this.tabMap.set(tab.id, this.formatTab(tab));
+    }
+
+    onDetached(tabId) {
+        if (this.tabMap.has(tabId)) {
+            this.tabMap.delete(tabId);
+        }
+    }
+
+    onUpdated(tabId, changeInfo, tab) {
+        if (!this.tabMap.has(tabId)) {
+            return;
+        }
+
+        if (!changeInfo.url && !changeInfo.title) {
+            return;
+        }
+
+        this.tabMap.set(tabId, this.formatTab(tab));
+    }
+
+    onRemoved(tabId) {
+        if (!this.tabMap.has(tabId)) {
+            return;
+        }
+
+        this.tabMap.delete(tabId);
     }
 
     searchIndex(searchText, sender, sendResponse) {
-        sendResponse(this.jsSearch.search(searchText));
+        sendResponse(fuzzy.filter(
+            searchText,
+            this.tabMapValueArray,
+            this.fuzzyOptions
+        ).map(match => match.original));
     }
 
-    init() {
-        this.updateSearchIndex();
+    async init() {
+        const currentTabs = await tabs.query({ currentWindow: true });
+        for(const tab of currentTabs) {
+            this.tabMap.set(tab.id, this.formatTab(tab));
+        }
     }
 }
 
